40febd34574fe18c89b26507a399edefdcf54419
Alejandra <maria.alejandra.fauquie@gmail.com>
2025-08-28 23:16:58 -0300

    Primer comit

---
+++ models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, BigInteger, DateTime, func, UniqueConstraint
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class SuscripcionTema(Base):
+    __tablename__ = "suscripciones_tema"
+    __table_args__ = (UniqueConstraint('user_id', 'tema'),)
+    suscripcion_id = Column(Integer, primary_key=True, index=True)
+    fecha = Column(DateTime(timezone=True), server_default=func.now())
+    user_id = Column(BigInteger, nullable=False, index=True)
+    tema = Column(String(50), nullable=False)
+
+class SuscripcionDataset(Base):
+    __tablename__ = "suscripciones_dataset"
+    __table_args__ = (UniqueConstraint('user_id', 'dataset'),)
+    suscripcion_id = Column(Integer, primary_key=True, index=True)
+    fecha = Column(DateTime(timezone=True), server_default=func.now())
+    user_id = Column(BigInteger, nullable=False, index=True)
+    dataset = Column(String(255), nullable=False)
+
+
---
+++ validators.py
@@ -0,0 +1,33 @@
+from urllib.parse import urlparse
+
+
+def extract_dataset_from_url(url):
+
+    parsed = urlparse(url)
+    path = parsed.path
+    prefix = "/dataset/"
+    if path.startswith(prefix):
+        return path[len(prefix):]
+    return None
+
+def valid_theme(input,superthemes):
+        if input.lower() in superthemes:
+            return True
+        else:
+            return False
+
+def valid_dataset(url):
+    try:
+        parsed = urlparse(url)
+        if parsed.scheme not in ("http", "https"):
+            return False
+        if parsed.netloc != "datos.gob.ar":
+            return False
+        if not parsed.path.startswith("/dataset/"):
+            return False
+        return True
+    except:
+        return False
+
+def dataset_exists(dataset,av_datasets):
+   pass
